73903546c6a40eaacfd1f5b276050d2d5f03c635
Optimus91 <optimus6199@hotmail.com>
2016-07-19 13:07:53 -0500

    Consolidated all get methods

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Error404_Hardware_Tier1.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Error404_Hardware_Tier1.java
@@ -20,172 +20,48 @@ public class Error404_Hardware_Tier1 extends OpMode {
     //  driver station.  If device isn't found   //
     //      the telemetry will print "null".     //
     *//////////////////////////////////////////////
-    public String lf_Get_Power_Tele() {
+
+    public String get_Power_Tele(DcMotor motor)
+    {
         String motor_return = "";
-
-        if (leftFront != null) {
-            motor_return += leftFront.getPower();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-
-    }  //get power for leftFront motor
-
-    public String rf_Get_Power_Tele() {
-
-        String motor_return = "";
-        if (rightFront != null) {
-            motor_return += rightFront.getPower();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-
-    }  //get power for rightFront motor
-
-    public String lr_Get_Power_tele() {
-
-        String motor_return = "";
-
-        if (leftRear != null) {
-            motor_return += leftRear.getPower();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-
-    }  //get power for leftRear motor
-
-    public String rr_Get_Power_Tele() {
-        String motor_return = "";
-        if (rightRear != null) {
-            motor_return += rightRear.getPower();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }  //get power for rightRear motor
-
-    public String lf_Get_Position_Tele() {
-        String motor_return = "";
-
-        if (leftFront != null) {
-            motor_return += leftFront.getCurrentPosition();
+        if(motor != null){
+            motor_return += motor.getPower();
             return motor_return;
         }
         motor_return += "NULL";
         return motor_return;
     }
 
-    public String rf_Get_Position_Tele() {
+    public String get_Position_Tele(DcMotor motor)
+    {
         String motor_return = "";
-
-        if (rightFront != null) {
-            motor_return += rightFront.getCurrentPosition();
-            return motor_return;
-        }
-        motor_return += "null";
-        return motor_return;
-    }
-
-    public String lr_Get_Position_Tele() {
-        String motor_return = "";
-
-        if (leftRear != null) {
-            motor_return += leftRear.getCurrentPosition();
+        if(motor != null)
+        {
+            motor_return += motor.getCurrentPosition();
             return motor_return;
         }
         motor_return += "NULL";
         return motor_return;
     }
 
-
-    public String rr_Get_Position_Tele() {
+    public String get_Mode_Tele(DcMotor motor)
+    {
         String motor_return = "";
-
-        if (rightRear != null) {
-            motor_return += rightRear.getCurrentPosition();
+        if(motor != null)
+        {
+            motor_return += motor.getMode();
             return motor_return;
         }
         motor_return += "NULL";
         return motor_return;
     }
 
-    public String lf_Get_Mode_Tele() {
+    public String get_Direction_Tele(DcMotor motor)
+    {
         String motor_return = "";
-        if (leftFront != null) {
-            motor_return += leftFront.getMode();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String rf_Get_Mode_Tele() {
-        String motor_return = "";
-        if (rightFront != null) {
-            motor_return += rightFront.getMode();
-            return motor_return;
-        }
-        motor_return += "null";
-        return motor_return;
-    }
-
-    public String lr_Get_Mode_Tele() {
-        String motor_return = "";
-        if (leftRear != null) {
-            motor_return += leftRear.getMode();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String rr_Get_Mode_Tele() {
-        String motor_return = "";
-        if (rightRear != null) {
-            motor_return += rightRear.getMode();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String lf_Get_Direction_Tele() {
-        String motor_return = "";
-        if (leftFront != null) {
-            motor_return += leftFront.getDirection();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String rf_Get_Direction_Tele() {
-        String motor_return = "";
-        if (rightFront != null) {
-            motor_return += rightFront.getDirection();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String lr_Get_Direction_Tele() {
-        String motor_return = "";
-        if (leftRear != null) {
-            motor_return += leftRear.getDirection();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String rr_Get_Direction_Tele() {
-        String motor_return = "";
-        if (rightRear != null) {
-            motor_return += rightRear.getDirection();
+        if(motor != null)
+        {
+            motor_return += motor.getDirection();
             return motor_return;
         }
         motor_return += "NULL";
@@ -198,156 +74,42 @@ public class Error404_Hardware_Tier1 extends OpMode {
     //      things, but not printed out.      //
     *///////////////////////////////////////////
 
-    int lf_Get_Power() {
-        int motor_return = 0;
-        if (leftFront != null) {
-            motor_return += leftFront.getPower();
+    public double get_Power(DcMotor motor)
+    {
+        double motor_return = 0;
+        if(motor != null)
+        {
+            motor_return = motor.getPower();
             return motor_return;
         }
         return motor_return;
     }
 
-    int rf_Get_Power() {
+    public int get_Position(DcMotor motor)
+    {
         int motor_return = 0;
-        if (rightFront != null) {
-            motor_return += rightFront.getPower();
+        if (motor != null)
+        {
+            motor_return = motor.getCurrentPosition();
             return motor_return;
         }
         return motor_return;
     }
 
-    int lr_Get_Power() {
-        int motor_return = 0;
-        if (leftRear != null) {
-            motor_return += leftRear.getPower();
-            return motor_return;
-        }
-        return motor_return;
-    }
-
-    int rr_Get_Power() {
-        int motor_return = 0;
-        if (rightRear != null) {
-            motor_return += rightRear.getPower();
-            return motor_return;
-        }
-        return motor_return;
-    }
-
-    public int lf_Get_Position() {
-        int motor_return = 0;
-
-        if (leftFront != null) {
-            motor_return += leftFront.getCurrentPosition();
-            return motor_return;
-        }
-        return motor_return;
-    }
-
-    public int rf_Get_Position() {
-        int motor_return = 0;
-
-        if (rightFront != null) {
-            motor_return += rightFront.getCurrentPosition();
-            return motor_return;
-        }
-        return motor_return;
-    }
-
-    public int lr_Get_Position() {
-        int motor_return = 0;
-
-        if (leftRear != null) {
-            motor_return += leftRear.getCurrentPosition();
-            return motor_return;
-        }
-        return motor_return;
-    }
-
-    public int rr_Get_Position() {
-        int motor_return = 0;
-
-        if (rightRear != null) {
-            motor_return += rightRear.getCurrentPosition();
-            return motor_return;
-        }
-        return motor_return;
-    }
-
-    public String lf_Get_Mode() {
+    public String get_Mode(DcMotor motor) {
         String motor_return = "";
-        if (leftFront != null) {
-            motor_return += leftFront.getMode();
+        if (motor != null) {
+            motor_return += motor.getMode();
             return motor_return;
         }
         motor_return += "NULL";
         return motor_return;
     }
 
-    public String rf_Get_Mode() {
+    public String get_Direction(DcMotor motor) {
         String motor_return = "";
-        if (rightFront != null) {
-            motor_return += rightFront.getMode();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String lr_Get_Mode() {
-        String motor_return = "";
-        if (leftRear != null) {
-            motor_return += leftRear.getMode();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String rr_Get_Mode() {
-        String motor_return = "";
-        if (rightRear != null) {
-            motor_return += rightRear.getMode();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String lf_Get_Direction() {
-        String motor_return = "";
-        if (leftFront != null) {
-            motor_return += leftFront.getDirection();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String rf_Get_Direction() {
-        String motor_return = "";
-        if (rightFront != null) {
-            motor_return += rightFront.getDirection();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String lr_Get_Direction() {
-        String motor_return = "";
-        if (leftRear != null) {
-            motor_return += leftRear.getDirection();
-            return motor_return;
-        }
-        motor_return += "NULL";
-        return motor_return;
-    }
-
-    public String rr_Get_Direction() {
-        String motor_return = "";
-        if (rightRear != null) {
-            motor_return += rightRear.getDirection();
+        if (motor != null) {
+            motor_return += motor.getDirection();
             return motor_return;
         }
         motor_return += "NULL";
@@ -360,40 +122,13 @@ public class Error404_Hardware_Tier1 extends OpMode {
     //          returns false.              //
     */////////////////////////////////////////
 
-    public boolean lf_Is_Encoder_Reached(int goal)
+    public boolean is_Encoder_Reached(int goal, DcMotor motor)
     {
-            int encoder_count = lf_Get_Position();
-            if(encoder_count == goal)
-            {return true;}
-            else
-            {return false;}
-    }
-
-    public boolean rf_Is_Encoder_Reached(int goal)
-    {
-            int encoder_count = rf_Get_Position();
-            if(encoder_count == goal)
-            {return true;}
-            else
-            {return false;}
-    }
-
-    public boolean lr_Is_Encoder_Reached(int goal)
-    {
-            int encoder_count = lr_Get_Position();
-            if(encoder_count == goal)
-            {return true;}
-            else
-            {return false;}
-    }
-
-    public boolean rr_Is_Encoder_Reached(int goal)
-    {
-            int encoder_count = rr_Get_Position();
-            if(encoder_count == goal)
-            {return true;}
-            else
-            {return false;}
+        int encoder_count = get_Position(motor);
+        if(encoder_count == goal)
+        {return true;}
+        else
+        {return false;}
     }
 
     //////////////////////////////////////////////
@@ -401,66 +136,24 @@ public class Error404_Hardware_Tier1 extends OpMode {
     //      if found, else returns false        //
     */////////////////////////////////////////////
 
-
-    public boolean lf_Is_Encoder_Reset()
+    public boolean is_Encoder_Reset(DcMotor motor)
     {
-            if(lf_Get_Position() == 0)
-            {return true;}
-            else
-            {return false;}
+        if(get_Position(motor) == 0)
+        {return true;}
+        else
+        {return false;}
     }
-
-    public boolean rf_Is_Encoder_Reset()
-    {
-            if(rf_Get_Position() == 0)
-            {return true;}
-            else
-            {return false;}
-    }
-
-
-    public boolean lr_Is_Encoder_Reset()
-    {
-            if(lr_Get_Position() == 0)
-            {return true;}
-            else
-            {return false;}
-    }
-
-    public boolean rr_Is_Encoder_Reset()
-    {
-            if(rr_Get_Position() == 0)
-            {return true;}
-            else
-            {return false;}
-    }
-
 
     ///////////////////////////////////////////
     /* methods that reset encoders if found, //
     //          else does nothing.           //
     *//////////////////////////////////////////
-    public void lf_Reset_Encoder() {
-        if (leftFront != null) {
-            leftFront.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        }
-    }  //reset leftFront encoder
 
-    public void rf_Reset_Encoder() {
-        if (rightFront != null) {
-            rightFront.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        }
-    }
-
-    public void lr_Reset_Encoder() {
-        if (leftRear != null) {
-            leftRear.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        }
-    }
-
-    public void rr_Reset_Encoder() {
-        if (rightRear != null) {
-            rightRear.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+    public void reset_Encoder(DcMotor motor)
+    {
+        if(motor != null)
+        {
+            motor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         }
     }
 
