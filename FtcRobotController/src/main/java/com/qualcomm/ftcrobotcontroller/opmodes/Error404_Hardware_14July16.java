574133b5e18c19e290c36a53aabf97af46487a96
nickdrones <nickbelbas@earthlink.net>
2016-07-19 10:44:36 -0500

    Made runner program for hardware class
    I removed the runner code from the hardware class and made a new
    program, Error404_Hardware_testing, to be a runner program for our code.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Error404_Hardware_14July16.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Error404_Hardware_14July16.java
@@ -6,11 +6,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
-/**
- * Created by optim on 7/13/2016.
- * This is a test comment inserted by coach Belbas.  hello
- *
- */
 public class Error404_Hardware_14July16 extends OpMode {
     DcMotor leftFront;
     DcMotor rightFront;
@@ -22,20 +17,9 @@ public class Error404_Hardware_14July16 extends OpMode {
     }
 
     public void start() {
-        set_direction(rightFront, "F");
-        set_power(1.0, rightFront);
-        set_mode(rightFront, "RTP");
-        set_position(rightFront, 1140);
-        rf_Reset_Encoder();
-        set_power(.2, rightFront);
-        set_position(rightFront, 1140);
     }
 
     public void loop() {
-        telemetry.addData("00 rf Power", rf_Get_Power_Tele());
-        telemetry.addData("02 rf Direction", rf_Get_Direction_Tele());
-        telemetry.addData("03 rf Mode", rf_Get_Mode_Tele());
-        telemetry.addData("01 rf Position", rf_Get_Position_Tele());
     }
 
     public void stop() {
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Error404_Hardware_Testing.java
@@ -0,0 +1,20 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+
+public class Error404_Hardware_Testing extends Error404_Hardware_14July16 {
+    public void init() {
+        hardware_map();
+    }
+
+    public void start() {
+    }
+
+    public void loop() {
+    }
+
+    public void stop() {
+    }
+}
