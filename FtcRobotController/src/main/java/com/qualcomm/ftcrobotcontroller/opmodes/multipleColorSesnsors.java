5d64cacaec6936e50ea6de7861b0152a11e88fe8
clarmom <clarissabelbas@gmail.com>
2016-11-01 00:27:18 -0500

    Working on two color sensors
    Trying to control sensor LED and get readings from both sensors.
    Changed the addresses back to 0x3c and 0x4c and am now getting good
    readings.  LED on/off seems a bit tricky.  Appears that LED
    enable/disable commands need to be scattered in various places to get it
    to work correctly.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/multipleColorSesnsors.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/multipleColorSesnsors.java
@@ -6,7 +6,7 @@ import android.widget.BaseExpandableListAdapter;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsUsbDeviceInterfaceModule;
-import com.qualcomm.robotcore.hardware.I2cController;
+import com.qualcomm.robotcore.hardware.I2cDevice;
 import com.qualcomm.robotcore.hardware.I2cControllerPortDeviceImpl;
 import com.qualcomm.robotcore.util.RobotLog;
 import com.qualcomm.robotcore.util.TypeConversion;
@@ -28,9 +28,9 @@ public class multipleColorSesnsors extends LinearOpMode {
         // Assumes config file matches these deignations
         //ie: mr, ods, touch, ir
         RGB = hardwareMap.colorSensor.get("mr");
-        RGB.setI2cAddress(30);       //30 is the decimal conversion from 7 bit hexadecimal value 0x1e converted from 8 bit hexadecimal 0x3c
+        RGB.setI2cAddress(0x3C);       //30 is the decimal conversion from 7 bit hexadecimal value 0x1e converted from 8 bit hexadecimal 0x3c
         beacon = hardwareMap.colorSensor.get("beacon");
-        beacon.setI2cAddress(38);       //38 is the decimal conversion from 7 bit hexadecimal value 0x26 converted from 8 bit hexadecimal 0x4c
+        beacon.setI2cAddress(0x4C);       //38 is the decimal conversion from 7 bit hexadecimal value 0x26 converted from 8 bit hexadecimal 0x4c
 
 //        beacon = hardwareMap.colorSensor.get("beacon");
 //        beacon.setI2cAddress(0x26);
@@ -43,14 +43,21 @@ public class multipleColorSesnsors extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()) {
+            RGB.enableLed(true);
+            beacon.enableLed(false);
 
-            telemetry.addData("01 - White down", RGB.red());
-            telemetry.addData("01.5 - I2C down", RGB.getI2cAddress());
-            telemetry.addData("02 - White out", beacon.red());
-            telemetry.addData("02.5 - I2C out", beacon.getI2cAddress());
+            telemetry.addData("01.1 - red down", RGB.red());
+            telemetry.addData("01.2 - blue down", RGB.blue());
+            telemetry.addData("01.3 - white down", RGB.alpha());
+            telemetry.addData("01 - I2C down", RGB.getI2cAddress());
+            telemetry.addData("02.1 - red out", beacon.red());
+            telemetry.addData("02.2 - blue out", beacon.blue());
+            telemetry.addData("02 - I2C out", beacon.getI2cAddress());
 
             // wait a hardware cycle before iterating.
             waitOneFullHardwareCycle();
+
         }
+
     }
-}
+   }
